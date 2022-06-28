1c726b8e1cb19ba22af9a0a33284e299122e0bda
afoggs <afoggs@squaddd.com>
2022-06-28 16:01:45 -0500

    Completed minimum problem.

---
+++ ch-03/minimum.js
@@ -0,0 +1,13 @@
+const minimum = (a, b) => {
+    if (a < b) {
+        console.log(b)
+    }
+    else {
+        console.log(b)
+    }
+}
+
+//Test Cases
+minimum(3,7);
+minimum(-10, 10)
+minimum(27, 5)
