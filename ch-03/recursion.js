baf12b4592794734b06d2c566e0263cc58db2622
afoggs <afoggs@squaddd.com>
2022-06-29 07:28:20 -0500

    Added book answer.

--- ch-03/recursion.js
+++ ch-03/recursion.js
@@ -27,3 +27,18 @@ isEven(-1);
 // → false
 isEven(-20);
 // -> true
+
+//JS Book Answer | Multiplies n by negative to reduce calls.
+// function isEven(n) {
+//     if (n == 0) return true;
+//     else if (n == 1) return false;
+//     else if (n < 0) return isEven(-n);
+//     else return isEven(n - 2);
+//   }
+  
+//   console.log(isEven(50));
+//   // → true
+//   console.log(isEven(75));
+//   // → false
+//   console.log(isEven(-1));
+//   // → false
