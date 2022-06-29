d0e2275d0de0f987a6d818456a897a4c6f4fcddf
afoggs <afoggs@squaddd.com>
2022-06-29 11:22:13 -0500

    Added JS Book Answer.

--- ch-04/sum.js
+++ ch-04/sum.js
@@ -33,3 +33,30 @@ range(1, 10)
 range(5, 2, -1)
 sum(range(1, 10))
 sum(range(5, 2, -1))
+
+//JS Book Answer | This accommodates for if someone doesn't define a step value for a range in which the start and end is reversed.
+// function range(start, end, step = start < end ? 1 : -1) {
+//     let array = [];
+  
+//     if (step > 0) {
+//       for (let i = start; i <= end; i += step) array.push(i);
+//     } else {
+//       for (let i = start; i >= end; i += step) array.push(i);
+//     }
+//     return array;
+//   }
+  
+//   function sum(array) {
+//     let total = 0;
+//     for (let value of array) {
+//       total += value;
+//     }
+//     return total;
+//   }
+  
+//   console.log(range(1, 10))
+//   // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//   console.log(range(5, 2, -1));
+//   // → [5, 4, 3, 2]
+//   console.log(sum(range(1, 10)));
+//   // → 55
