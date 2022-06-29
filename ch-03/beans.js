58201bdbb5e7f11a310264c763a0bd23b5d3bde8
afoggs <afoggs@squaddd.com>
2022-06-29 07:34:32 -0500

    Added js book answer.

--- ch-03/beans.js
+++ ch-03/beans.js
@@ -18,6 +18,22 @@ const countChar = (string, char) => {
     console.log(count);
 }
 
+//test cases.
 countBs("BBC")
 countChar("kakkerlak", "k")
 countChar("antoine", "n")
+
+// //JS Book Answer
+// function countChar(string, ch) {
+//     let counted = 0;
+//     for (let i = 0; i < string.length; i++) {
+//       if (string[i] == ch) {
+//         counted += 1;
+//       }
+//     }
+//     return counted;
+//   }
+  
+//   function countBs(string) {
+//     return countChar(string, "B");
+//   }
